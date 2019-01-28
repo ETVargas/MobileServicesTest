16daf9b6d8ed1d174dc10a1daa7c73ba265881d7
ETVargas <evargas30@gatech.edu>
2019-01-28 06:43:19 -0500

    Implemented example weather feature.

--- src/main/java/com/mas/ethan/mobileservicestest/EthanActivity.java
+++ src/main/java/com/mas/ethan/mobileservicestest/EthanActivity.java
@@ -1,7 +1,9 @@
 package com.mas.ethan.mobileservicestest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class EthanActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class EthanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ethan);
+    }
+
+
+    public void moveToWeatherScreen(View v) {
+        Intent intentCapture = new Intent(EthanActivity.this, WeatherActivity.class);
+        startActivity(intentCapture);
     }
 }
