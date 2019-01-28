20656898a3b6df64cc7441bbcbfa74c6fee995a2
ETVargas <evargas30@gatech.edu>
2019-01-28 15:40:25 -0500

    Implemented example location feature.

--- src/main/java/com/mas/ethan/mobileservicestest/EthanActivity.java
+++ src/main/java/com/mas/ethan/mobileservicestest/EthanActivity.java
@@ -18,4 +18,9 @@ public class EthanActivity extends AppCompatActivity {
         Intent intentCapture = new Intent(EthanActivity.this, WeatherActivity.class);
         startActivity(intentCapture);
     }
+
+    public void moveToLocationScreen(View v) {
+        Intent intentCapture = new Intent(EthanActivity.this, LocationActivity.class);
+        startActivity(intentCapture);
+    }
 }
