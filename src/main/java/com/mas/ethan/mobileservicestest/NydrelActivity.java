c074208da6106b0b51e82ea3b2fa59eeb32aa9ce
Drel22 <Drel22@users.noreply.github.com>
2019-01-30 11:51:20 -0500

    Add files via upload
    Added objects to form to collect data

--- src/main/java/com/mas/ethan/mobileservicestest/NydrelActivity.java
+++ src/main/java/com/mas/ethan/mobileservicestest/NydrelActivity.java
@@ -2,12 +2,30 @@ package com.mas.ethan.mobileservicestest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+
 
 public class NydrelActivity extends AppCompatActivity {
+    private EditText nameField;
+    private EditText foodField;
+    private Button pushButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nydrel);
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_nydrel);
+            nameField = (EditText) findViewById(R.id.EditTextName);
+            foodField = (EditText) findViewById(R.id.EditTextFood);
+            pushButton  = findViewById(R.id.ButtonSendInfo);
+        }
+
+    public void sendInfo(View button) {
+        // Do click handling here
+        String name = nameField.getText().toString();
+        String food = foodField.getText().toString();
     }
 }
